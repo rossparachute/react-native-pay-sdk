31d3ce7a570e23380f81fc81b4ee651dc607da72
Peter Carpenter <peter.carpenter@dojo.tech>
2023-03-21 11:40:19 +0000

    chore: comment out ios implementation

--- ios/DojoReactNativePaySdk.mm
+++ ios/DojoReactNativePaySdk.mm
@@ -1,6 +1,6 @@
 #import "DojoReactNativePaySdk.h"
 #import <React/RCTUtils.h>
-#import <dojo_ios_sdk_drop_in_ui-Swift.h>
+@import dojo_ios_sdk_drop_in_ui;
 
 @implementation DojoReactNativePaySdk
 RCT_EXPORT_MODULE()
@@ -8,37 +8,37 @@ RCT_EXPORT_MODULE()
 // Example method
 // See // https://reactnative.dev/docs/native-modules-ios
 RCT_REMAP_METHOD(startPaymentFlow, startPaymentFlow
-                 : (JS::NativeDojoReactNativePaySdk::PaymentDetails &)
+                 : (NSDictionary &)
                      details resolve
                  : (RCTPromiseResolveBlock)resolve reject
                  : (RCTPromiseRejectBlock)reject) {
 
   DojoSDKDropInUI *dojoUI = [[DojoSDKDropInUI alloc] init];
 
-  UIViewController *vc = RCTPresentedViewController();
-
-  DojoUIApplePayConfig *applePayConfig = nil;
-  if (details.applePayMerchantId != nil) {
-    applePayConfig = [[DojoUIApplePayConfig alloc]
-        initWithMerchantIdentifier:details.applePayMerchantId];
-  }
-
-  DojoThemeSettings *theme;
-  if (details.darkTheme == true) {
-    theme = [DojoThemeSettings getDarkTheme];
-  } else {
-    theme = [DojoThemeSettings getLightTheme];
-  }
-
-  [dojoUI startPaymentFlowWithPaymentIntentId:details.intentId
-                                   controller:vc
-                               customerSecret:customerSecret
-                               applePayConfig:applePayConfig
-                                themeSettings:theme
-                                   completion:^(NSInteger result) {
-                                     NSLog(@"%ld", (long)result);
-                                     resolve(result);
-                                   }];
+//  UIViewController *vc = RCTPresentedViewController();
+//
+//  DojoUIApplePayConfig *applePayConfig = nil;
+//  if (details.applePayMerchantId != nil) {
+//    applePayConfig = [[DojoUIApplePayConfig alloc]
+//        initWithMerchantIdentifier:details.applePayMerchantId];
+//  }
+//
+//  DojoThemeSettings *theme;
+//  if (details.darkTheme == true) {
+//    theme = [DojoThemeSettings getDarkTheme];
+//  } else {
+//    theme = [DojoThemeSettings getLightTheme];
+//  }
+//
+//  [dojoUI startPaymentFlowWithPaymentIntentId:details.intentId
+//                                   controller:vc
+//                               customerSecret:customerSecret
+//                               applePayConfig:applePayConfig
+//                                themeSettings:theme
+//                                   completion:^(NSInteger result) {
+//                                     NSLog(@"%ld", (long)result);
+//                                     resolve(result);
+//                                   }];
 }
 
 // Don't compile this code when we build for the old architecture.
