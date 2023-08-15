0fa019a0adededad691c20876e27096785bcb0ee
sadia1288 <sadiahussain118@gmail.com>
2023-08-15 16:18:03 +0100

    Add Segment Control to Settings page

--- example/src/FirstPage.js
+++ example/src/FirstPage.js
@@ -12,79 +12,76 @@ import { startPaymentFlow } from '@dojo-engineering/react-native-pay-sdk';
 import { useSwitchContext } from './SwitchContext';
 
 const FirstPage = () => {
-    const [userInput, setUserInput] = useState ('');
+  const [userInput, setUserInput] = useState('');
 
-    const handleChange = (value) => {
-      setUserInput(value);
-    }
-  
-    const handlePress = () => {
-       pay();
-    }
+  const handleChange = (value) => {
+    setUserInput(value);
+  }
 
-    const pay = () => {
-      startPaymentFlow({
-        intentId: userInput,
-      }).then((res) => {
-        setResult(res);
-      });
-    };
+  const handlePress = () => {
+    pay();
+  }
 
-    const navigation = useNavigation();
-    
-    const { isEnabled } = useSwitchContext();
+  const pay = () => {
+    startPaymentFlow({
+      intentId: userInput,
+    }).then((res) => {
+      setResult(res);
+    });
+  };
 
+  const navigation = useNavigation();
+  const { isEnabled, selectedIndex } = useSwitchContext();
 
-    return (
-      <View style={styles.mainContainer}>
-          <TextInput 
-            style = {styles.input}
-            onChangeText={handleChange} 
-          />
-          <Button 
-            title="Submit" 
-            color='green'
-            onPress={handlePress}
-          />
-          <Text>{isEnabled? "GPay Enabled": "GPay Disabled" } </Text> 
-
-
-        <View style={styles.bottomContainer}>
-          <Button 
-            title="Settings"
-            type='clear'
-            color='gray'
-            hide='true'
-            onPress = {() => navigation.navigate('Settings')}
-          />
-        </View>
-
-        <StatusBar style="auto" />
+  return (
+    <View style={styles.mainContainer}>
+      <TextInput
+        style={styles.input}
+        onChangeText={handleChange}
+      />
+      <Button
+        title="Submit"
+        color='#008275'
+        onPress={handlePress}
+      />
+      <View style={{ margin: 10, alignItems: 'center', }}>
+        <Text>{isEnabled ? "GPay Enabled" : "GPay Disabled"} </Text>
+        <Text>Theme: {selectedIndex} </Text>
       </View>
-    );
+      <View style={styles.bottomContainer}>
+        <Button
+          title="Settings"
+          type='clear'
+          hide='true'
+          onPress={() => navigation.navigate('Settings')}
+        />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    mainContainer: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      margin: 10,
-    },
-  
-    bottomContainer: {
-      position: 'absolute',
-      bottom: 0,
-      display: "none"
-    },
-  
-    input: {
-      borderWidth:1,
-      borderColor: 'gray', 
-      margin:10, 
-      width: 200,
-    },
-  });
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+  },
+
+  bottomContainer: {
+    position: 'absolute',
+    bottom: 0,
+    display: "none"
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    margin: 10,
+    width: 200,
+  },
+});
 
 export default FirstPage;
--- example/src/SettingsPage.js
+++ example/src/SettingsPage.js
@@ -1,35 +1,65 @@
 import React from 'react';
-import { Text, View, StyleSheet, Switch} from 'react-native';
-
+import { Text, View, StyleSheet, Switch } from 'react-native';
 import { useSwitchContext } from './SwitchContext';
+import SegmentedControlTab from 'react-native-segmented-control-tab';
 
-const SettingsPage = ({}) => {
+const SettingsPage = () => {
 
-    const { isEnabled, toggleSwitch } = useSwitchContext();
+    const { isEnabled, toggleSwitch, selectedIndex, handleIndexChange } = useSwitchContext();
 
     return (
-        <View style = {styles.mainContainer} >
-            <Text style = {styles.label} >GPay</Text>
-            <Switch
-                name = "GPay switch"
-                onValueChange={toggleSwitch}
-                value={isEnabled}
-            />
+        <View style={styles.mainContainer} >
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.label} >GPay</Text>
+                <Switch style={{ margin: 10, color: 'blue' }}
+                    name="GPay switch"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                />
+            </View>
+            <View style={styles.segmentContainer} >
+                <SegmentedControlTab
+                    values={['Light Theme', 'Dark Theme']}
+                    selectedIndex={selectedIndex}
+                    onTabPress={handleIndexChange}
+                    activeTabStyle={styles.activeTabStyle}
+                    tabStyle={styles.tabStyle}
+                    tabTextStyle={styles.tabTextStyle}
+                />
+            </View>
         </View>
     );
-} 
+}
 
 const styles = StyleSheet.create({
     mainContainer: {
-        padding:80,
+        padding: 80,
         justifyContent: "center",
         alignItems: "center"
     },
     label: {
-        margin:10,
-        fontSize:22,
-        fontWeight: 'bold'
-    }
+        margin: 10,
+        fontSize: 17,
+    },
+    segmentContainer: {
+        borderRadius: 8,
+        paddingHorizontal: 10,
+        marginTop: 20,
+        width: 200
+    },
+    activeTabStyle: {
+        backgroundColor: '#008275',
+        borderColor: 'white',
+
+    },
+    tabStyle: {
+        backgroundColor: 'white',
+        borderColor: 'white',
+    },
+    tabTextStyle: {
+        color: '#008275',
+    },
+
 
 
 })
